84db96f8937a46b6720aaab61dc6e22fc454ba2e
bogasa <asaftei.bogdan@gmail.com>
2021-03-08 17:55:31 +0200

    Add project files.

---
+++ InterfaceDemo/ClassA.cs
@@ -0,0 +1,33 @@
+﻿using System;
+
+namespace InterfaceDemo
+{   
+    class ClassA : IShape
+    {
+        private int myNumber;
+
+        public int MyNumber
+        {
+            get
+            {
+                return myNumber;
+            }
+            set
+            {
+                if (value < 0)
+                {
+                    myNumber = 0;
+                }
+                else
+                {
+                    myNumber = value;
+                }
+            }
+        }
+
+        public void InterfaceMethod()
+        {
+            Console.WriteLine("The number is {0}.", myNumber);
+        }
+    }
+}
---
+++ InterfaceDemo/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace InterfaceDemo
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var a = new ClassA();
+            a.MyNumber = 5;
+            a.InterfaceMethod();
+            Console.Read(); 
+        }
+    }
+
+    interface IShape
+    {
+        public int MyNumber { get; set; }
+        void InterfaceMethod();
+    }
+}
